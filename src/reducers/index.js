9738bd2e8bf801b9b5a53b9004f0dca091fa5ca6
padrisimo <adrianfernandezchamizo@gmail.com>
2018-10-15 00:16:06 +0200

    add redux

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  items:[]
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+
+  case 'typeName':
+    return { ...state }
+
+  default:
+    return state
+  }
+}
