d627996f4b114db345adba6b1354fd02efcfe23b
padrisimo <adrianfernandezchamizo@gmail.com>
2018-10-17 00:15:21 +0200

    use actions to modify state

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+import { loadStories, clear } from '../actions'
 
-export const Stories = props => <pre>
-  <code>{JSON.stringify(props, null, 2)}</code>
-</pre>
+const Stories = props => (
+  <div>
+    <button type="button" onClick={props.loadStories}>Load Top 3 Stories</button>
+    <button type="button" onClick={props.clear}>Clear</button>
+    <StoryList {...props} />
+  </div>
+);
+
+const StoryList = ({items}) => {
+  if (items.length === 0) return null;
+  return (
+    <div>
+      {items.map(item => <Story {...item} key={item.id} />)}
+    </div>
+  )
+}
+
+const Story = ({title}) =>  <p>{title}</p>;
 
 const mapState = state => state;
 
-export default connect(mapState)(Stories);
+export default connect(mapState, { loadStories, clear })(Stories);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,41 @@
+import { LOAD_STORIES, CLEAR_STORIES } from "../actions";
+
+const stories = [
+  {
+    "by": "bleakgadfly",
+    "id": 14967192,
+    "title": "To Protect Voting, Use Open-Source Software",
+    "url": "https://mobile.nytimes.com/2017/08/03/opinion/open-source-software-hacker-voting.html"
+  },
+  {
+    "by": "mtyurt",
+    "id": 14966545,
+    "title": "Git: Using Advanced Rebase Features for a Clean Repository",
+    "url": "https://mtyurt.net/2017/08/08/git-using-advanced-rebase-features-for-a-clean-repository/"
+  },
+  {
+    "by": "callumlocke",
+    "id": 14967335,
+    "title": "Inside an AI brain: What does machine learning look like?",
+    "url": "https://www.graphcore.ai/posts/what-does-machine-learning-look-like"
+  }
+];
+
+
 const initialState = {
-  items:[]
+  items: []
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-
-  case 'typeName':
-    return { ...state }
-
-  default:
-    return state
+    case LOAD_STORIES:
+      return {
+        items: stories.slice(),
+      };
+    case CLEAR_STORIES:
+      return {
+        items: [],
+      };
+    default: return state;
   }
 }
