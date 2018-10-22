bf686980d8c06656aa89b3cc43a3ee77cdd29d41
padrisimo <adrianfernandezchamizo@gmail.com>
2018-10-22 19:03:47 +0200

    done adding redux-observable to a redux app

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,4 +1,4 @@
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { combineEpics, ofType } from 'redux-observable';
 import { switchMap, delay } from 'rxjs/operators';
 import { LOAD_STORIES, clear } from '../actions';
@@ -7,8 +7,7 @@ import { LOAD_STORIES, clear } from '../actions';
 const loadStoriesEpic = (action$) =>
   action$.pipe(
     ofType(LOAD_STORIES),
-    switchMap(() => of(clear()).pipe(delay(2000))
-    
+    switchMap(() => of(clear()).pipe(delay(2000)) 
     )
   )
 
